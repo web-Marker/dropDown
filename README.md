6b7508d1a3942a6c62a59e3c58ec36a6ca757f97
Mark <mark@MarkdeiMac.local>
2018-01-18 16:14:34 +0800

    What:增加下拉视觉差动画支持
    * 简短的描述干了什么
    
    Why:下拉可以有动画制定，结合gka模块生成的序列帧制作
    
    * 我为什么要这么做？
    
    How:
    
    * 我是怎么做的？这么做会有什么副作用？

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 下拉加载H5+WEB版本
 
-![](https://github.com/web-Marker/dropDown/blob/master/3.gif) 
+![](https://github.com/web-Marker/dropDown/blob/master/pic1.gif) 
 
 ## 下拉加载(持续更新react和vue组件版)
 
@@ -14,6 +14,33 @@
 * 下拉视差动画版本
 * react组件版本
 * vue组件版本
+
+## 更新支持视差动画(结合模块gka序列帧)
+
+![](https://github.com/web-Marker/dropDown/blob/master/pic2.gif) 
+
+
+* gka模块可以生成css3 canvas svg等序列帧动画代码,demo目前用的是css3，图片是gka自动生成的一个精灵图
+[gka官网地址](https://gka.js.org/#/?id=gka)
+
+```js
+	var drop = new drops({
+		el: document.body,//插入的节点
+		callback: function(){ //下拉刷新成功后的回调
+			var timer = setTimeout(function () {
+	            clearTimeout(timer);
+	            alert("下拉已经刷新, Do someThing")
+	        	drop.pullToRefreshDone();
+	        }, 500);
+		},
+		isFilter: false, //是否支付滤镜效果,
+		isAnima: true, //是否开启下拉动画
+		animaClass: 'animation', //动画类名
+		imgClassName: 'a', //css3动画序列类名首a1-an 就填写a就行
+		frameSize: '21'//动画一共有多少帧
+	})
+```
+
 
 ## 初始化调用方法(增加定时器模拟,实际开发可以剔除)
 
---
+++ anima-web/anima.js
@@ -0,0 +1,183 @@
+/*
+* @Author: mark
+* @Date:   2018-01-12 10:05:42
+* @Last Modified by:   mark
+* @Last Modified time: 2018-01-18 16:02:14
+* @Author: mark
+* @Date:   2017-03-09 15:50:03
+* @Last Modified by:   mark
+* @Last Modified time: 2017-03-14 18:50:48
+*/
+
+(function(global, factory) {
+	typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() : typeof define === "function" && define.amd ? define(factory) : (global.drops = factory())
+})(this, function() {
+
+	var drops = function(options) {
+		console.log(options.el.classList);
+		this.options = options;
+		this.contain = options.el;
+		this.distance = 50;
+		this.attachEvents(options.callback)
+	};
+
+	drops.prototype = {
+		//是否是移动端模式
+		support: function() {
+			return !! (("ontouchstart" in window) || window.DocumentTouch && document instanceof window.DocumentTouch)
+		},
+
+		//分别的几个触摸事件
+		touchEvents: function() {
+			return {
+				start: this.support() ? "touchstart": "mousedown",
+				move: this.support() ? "touchmove": "mousemove",
+				end: this.support() ? "touchend": "mouseup",
+			}
+		},
+
+		//移动坐标
+		getTouchPosition: function(e) {
+			if (this.support()) {
+				return {
+					x: e.targetTouches[0].pageX,
+					y: e.targetTouches[0].pageY
+				}
+			} else {
+				return {
+					x: e.pageX,
+					y: e.pageY
+				}
+			}
+		},
+
+		//触摸开始
+		touchStart: function(e) {
+			if (this.contain.classList.contains("refreshing")) {
+				return
+			}
+			var p = this.getTouchPosition(e);
+			this.start = p;
+			this.diffY = 0
+		},
+
+		//触摸移动
+		touchMove: function(e) {
+			if (this.contain.classList.contains("refreshing") || !this.start || (this.contain.scrollTop > 0)) {
+				return
+			}
+
+			var p = this.getTouchPosition(e);
+			this.diffY = p.y - this.start.y;
+
+			if (this.diffY < 0) {
+				return
+			}
+		
+			this.contain.classList.add("touching");
+
+			e.preventDefault();
+			e.stopPropagation();
+
+			this.diffY = Math.pow(this.diffY, 0.8);
+		
+			this.odiv.firstChild.style.height = this.diffY + "px";
+			var list = this.contain.classList;
+
+			//是否开启苹果滤镜模式
+			if (this.options.isFilter) {
+				this.contain.style.cssText = '-webkit-filter: blur('+(5 * (this.diffY) / 50)+'px);filter: blur('+(5 * (this.diffY) / 50)+'px);'
+			}
+
+			//开启序列帧动画模式
+			if (this.options.isAnima) {
+				var frameSize =  Math.floor(this.options.frameSize * (this.diffY) / 150);
+
+				if (frameSize > this.options.frameSize ) frameSize = this.options.frameSize;
+
+				var name = this.options.imgClassName + frameSize;
+			
+				document.querySelector('.'+this.options.animaClass).className = this.options.animaClass + ' ' + name;
+				
+			}
+
+			if (this.diffY < this.distance) {
+				list.remove("pull-up");
+				list.add("pull-down")
+			}else {
+				list.remove("pull-down");
+				list.add("pull-up")
+			}
+			return false
+		},
+
+		//触摸结束
+		touchEnd: function(callback) {
+			this.start = false;
+			if (this.diffY <= 0 || this.contain.classList.contains("refreshing")) {
+				return
+			}
+			var list = this.contain.classList;
+			list.remove("touching");
+			list.remove("pull-down");
+			list.remove("pull-up");
+			if (this.options.isFilter) {
+				this.contain.style.cssText = '-webkit-filter: blur(0px);filter: blur(0px);'
+			}
+			if (Math.abs(this.diffY) <= this.distance) {
+				this.odiv.firstChild.style.height = "0px"
+			}else {
+				this.odiv.firstChild.style.height = this.distance + "px";
+				list.add("refreshing");
+
+				if (this.options.isAnima) {
+					document.querySelector('.' + this.options.animaClass).classList.add('hide');
+				}
+
+				this.refresh(callback)
+			}
+			return false
+		},
+
+		//移动结束之后
+		refresh: function(callback) {
+			if (callback) {
+				callback()
+			}
+		},
+
+		//插入html代码
+		attachEvents: function(callback) {
+			var el = this.contain;
+			el.classList.add("dropload");
+
+			if (this.options.isAnima) {
+				var classList = '1';
+				var imgClassName = this.options.imgClassName + classList;
+			}
+			
+			var arrow = this.options.isAnima ? '<div class="'+this.options.animaClass+' '+imgClassName+'"></div>' : '<div class="arrow"></div>';
+
+			var tpl = ['<div class="dropload-layer">','<div class="inner">',''+arrow+'','<div class="loader"></div>','<div class="down">下拉刷新</div>','<div class="up">释放刷新</div>','<div class="refresh">正在刷新</div></div></div>'];
+
+			this.odiv = document.createElement("div");
+			this.odiv.innerHTML = tpl.join("");
+			el.insertBefore(this.odiv, el.firstChild);
+
+			el.addEventListener(this.touchEvents().start, this.touchStart.bind(this), this, false);
+			el.addEventListener(this.touchEvents().move, this.touchMove.bind(this), this, false);
+			el.addEventListener(this.touchEvents().end, this.touchEnd.bind(this, callback), this, false)
+		},
+
+		//松开之后
+		pullToRefreshDone: function() {
+			this.contain.classList.remove("refreshing");
+			if (this.options.isAnima) {
+				document.querySelector('.' + this.options.animaClass).classList.remove('hide');
+			}
+			this.contain.querySelector(".dropload-layer").style.height = "0px"
+		}
+	};
+
+	return drops
+});
